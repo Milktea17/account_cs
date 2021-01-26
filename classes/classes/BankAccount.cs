71f198be2efb0a458b36930d40548b44581636ea
unknown <llilljs@naver.com>
2021-01-26 19:11:07 +0900

    first commit

---
+++ classes/classes/BankAccount.cs
@@ -0,0 +1,86 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes
+{
+    class BankAccount
+    {
+        //private니까 클래스 내의 코드로만 액세스 가능
+        //static으로 객체내에서 공유됨
+        private static int accountNumberSeed = 1234567890; //데이터멤버
+
+        //property(3개) : get, set (getter, setter있음)
+        public string Number { get; }
+        public string Owner { get; set; }
+        public decimal Balance //속성영역
+        {                       //이 안에 get, set 정의를 해준다
+            get
+            {
+                decimal balance = 0;
+                foreach (var item in allTransaction)
+                {
+                    balance += item.Amount;
+                }
+                return balance;
+            }
+        }
+
+        //method(2개) : 단일함수를 수행하는 코드블록
+        public BankAccount(string name, decimal initialBalance) //생성자
+        {
+            this.Number = accountNumberSeed.ToString();
+            accountNumberSeed++;
+
+            this.Owner = name;
+            MakeDeposit(initialBalance, DateTime.Now, "Initial balance");
+        }
+
+        private List<Transaction> allTransaction = new List<Transaction>(); //거래기록 저장
+        
+        public void MakeDeposit(decimal amount, DateTime date, string note) //예금
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of deposit must be positive");
+            }
+            var deposit = new Transaction(amount, date, note);
+            allTransaction.Add(deposit);
+        }
+
+        public void MakeWithdrawal(decimal amount, DateTime date, string note) //인출
+        {
+            if (amount <= 0)
+            {
+                throw new ArgumentOutOfRangeException(nameof(amount), "Amount of withdrawal must be positive");
+            }
+            if (Balance - amount < 0)
+            {
+                throw new InvalidOperationException("Not sufficient funds for this withdrawal");
+            }
+
+            var withdrawal = new Transaction(-amount, date, note);
+            allTransaction.Add(withdrawal);
+        }
+
+        //트랜젝션 기록에 대해 string을 생성
+        public string GetAccountHistory()
+        {
+            //StringBuilder는 문자열을 조합할 때마다 새로운 변수를 생성할필요가 없다
+            var report = new System.Text.StringBuilder();
+
+            decimal balance = 0;
+            report.AppendLine("Date\t\tAmount\tBalance\tNote");
+            foreach (var item in allTransaction)
+            {
+                balance += item.Amount;
+                report.AppendLine($"{item.Date.ToShortDateString()}\t{item.Amount}\t{balance}\t{item.Notes}");
+            }
+
+            return report.ToString();
+        }
+
+    }
+}
---
+++ classes/classes/Program.cs
@@ -0,0 +1,46 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace classes
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            var account = new BankAccount("jj", 1000);
+            Console.WriteLine($"Account {account.Number} was created for {account.Owner} with {account.Balance} initial balance");
+            
+            account.MakeWithdrawal(500, DateTime.Now, "Rent Payment"); //인출 저장
+            Console.WriteLine(account.Balance); //입력된 allTransaction 기록으로 계산해서 출력
+            account.MakeDeposit(100, DateTime.Now, "Friend paid me back"); //예금 저장
+            Console.WriteLine(account.Balance);
+
+            /* test
+            try
+            {
+                var invalidAccount = new BankAccount("invalid", -55);
+            }
+            catch (ArgumentOutOfRangeException e)
+            {
+                Console.WriteLine("Exception caught creating account with negative balance ");
+                Console.WriteLine(e.ToString());
+            }
+            
+            try
+            {
+                account.MakeWithdrawal(750, DateTime.Now, "Attempt to overdraw");
+            }
+            catch (InvalidOperationException e)
+            {
+                Console.WriteLine("Exception caught trying to overdraw");
+                Console.WriteLine(e.ToString());
+            }
+            */
+
+            Console.WriteLine(account.GetAccountHistory());
+        }
+    }
+}
